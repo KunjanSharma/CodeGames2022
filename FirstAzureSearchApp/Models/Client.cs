025d06ea049e8c956916ad7775eeb08a8bfd8812
Kunjan Sharma <Kunjan.Sharma@wolterskluwer.com>
2022-06-08 14:43:15 -0700

    Add project files.

---
+++ FirstAzureSearchApp/Models/Client.cs
@@ -0,0 +1,35 @@
+﻿//using System;
+//using System.Text.Json.Serialization;
+//using Azure.Search.Documents.Indexes;
+//using Azure.Search.Documents.Indexes.Models;
+
+namespace FirstAzureSearchApp.Models
+{
+    public partial class Client
+    {
+        //[SimpleField(IsKey = true, IsFilterable = true)]
+        public string ClientID { get; set; }
+
+        //[SearchableField(IsSortable = true)]
+        public string ClientSubID { get; set; }
+
+        //[SearchableField(IsSortable = true)]
+        public string ClientSortName { get; set; }
+
+        //[SearchableField(AnalyzerName = LexicalAnalyzerName.Values.EnLucene)]
+        public string SubDescription { get; set; }
+
+        //[SearchableField(IsFilterable = true, IsSortable = true, IsFacetable = true)]
+        public string Office { get; set; }
+
+        //[SearchableField(IsFilterable = true, IsSortable = true, IsFacetable = true)]
+        public string BuisnessUnit { get; set; }
+
+        //[SearchableField(IsFilterable = true, IsFacetable = true)]
+        public string Region { get; set; }
+
+        //[SearchableField(IsFilterable = true, IsSortable = true, IsFacetable = true)]
+        public string ClientType { get; set; }
+     
+    }
+}
---
+++ FirstAzureSearchApp/Models/SearchData.cs
@@ -0,0 +1,17 @@
+﻿using Azure.Search.Documents.Models;
+using Microsoft.Azure.Search.Models;
+using System.Collections.Generic;
+
+namespace FirstAzureSearchApp.Models
+{
+    public class SearchData
+    {
+        // The text to search for.
+        public string searchText { get; set; }
+
+        public string totalCount { get; set; }
+        // The list of results.
+        public List<Client> resultList;
+
+    }
+}
